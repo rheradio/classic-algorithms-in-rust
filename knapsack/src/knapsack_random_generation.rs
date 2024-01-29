c24bbc6b9b9b3fbdc3a4d6706af0356d2cb07c0d
Ruben Heradio <rheradio@issi.uned.es>
2024-01-29 13:17:56 +0100

    Start knapsack

---
+++ knapsack/src/knapsack_random_generation.rs
@@ -0,0 +1,165 @@
+#[warn(dead_code)]
+#[warn(unused_imports)]
+use std::time::{SystemTime, UNIX_EPOCH};
+pub const NUM_ITEMS: i32 = 20; // A reasonable value for exhaustive search.
+pub const MIN_VALUE: i32 = 1;
+pub const MAX_VALUE: i32 = 10;
+pub const MIN_WEIGHT: i32 = 4;
+pub const MAX_WEIGHT: i32 = 10;
+
+pub struct Item {
+    pub value: i32,
+    pub weight: i32,
+    pub is_selected: bool,
+}
+
+// ************
+// *** Prng ***
+// ************
+// The Prng struct represents a pseudorandom number generator.
+// It has a seed value as its sole field
+pub struct Prng {
+    pub seed: u32,
+}
+
+impl Prng {
+    fn new() -> Self {
+        let mut prng = Self { seed: 0 };
+        prng.randomize();
+        return prng;
+    }
+
+    fn randomize(&mut self) {
+        let millis = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_millis();
+        self.seed = millis as u32;
+    }
+
+    // Return a pseudorandom value in the range [0, 2147483647].
+    fn next_u32(&mut self) -> u32 {
+        self.seed = self.seed.wrapping_mul(1_103_515_245).wrapping_add(12_345);
+        self.seed %= 1 << 31;
+        return self.seed;
+    }
+
+    // Return a pseudorandom value in the range [0.0, 1.0).
+    fn next_f64(&mut self) -> f64 {
+        let f = self.next_u32() as f64;
+        return f / (2147483647.0 + 1.0);
+    }
+
+    // Return a pseudorandom value in the range [min, max).
+    fn next_i32(&mut self, min: i32, max: i32) -> i32 {
+        let range = (max - min) as f64;
+        let result = min as f64 + range * self.next_f64();
+        return result as i32;
+    }
+}
+
+// Make some random items.
+// This function creates a vector to hold the new items.
+// Then for each item, it generates a random value between
+// min_value and max_value. Similarly, it generates a random
+// weight between min_weight and max_weight. It uses those
+// values to create a new Item struct, setting its is_selected
+// field to false. It then pushes the new Item onto the end of
+// the vector
+pub fn make_items(
+    prng: &mut Prng,
+    num_items: i32,
+    min_value: i32,
+    max_value: i32,
+    min_weight: i32,
+    max_weight: i32,
+) -> Vec<Item> {
+    let mut items: Vec<Item> = Vec::with_capacity(num_items as usize);
+    for _ in 0..num_items {
+        let item = Item {
+            value: prng.next_i32(min_value, max_value),
+            weight: prng.next_i32(min_weight, max_weight),
+            is_selected: false,
+        };
+        items.push(item);
+    }
+    return items;
+}
+
+// Return a copy of the items.
+// The copy_items function makes a copy of a vector of items and
+// returns it. We use this so each algorithm can have its own copy
+// of the items without messing up the vector for any later algorithms
+pub fn copy_items(items: &mut Vec<Item>) -> Vec<Item> {
+    let mut new_items: Vec<Item> = Vec::with_capacity(items.len());
+    for item in items {
+        let new_item = Item {
+            value: item.value,
+            weight: item.weight,
+            is_selected: item.is_selected,
+        };
+        new_items.push(new_item);
+    }
+    return new_items;
+}
+
+// Return the total value of the items.
+// If add_all is true, add up all items.
+// If add_all is false, only add up the selected items.
+pub fn sum_values(items: &mut Vec<Item>, add_all: bool) -> i32 {
+    if add_all {
+        return items.iter().map(|item| item.value).sum();
+    } else {
+        return items
+            .iter()
+            .filter(|item| item.is_selected)
+            .map(|item| item.value)
+            .sum();
+    }
+}
+
+// Return the total weight of the items.
+// If add_all is false, only add up the selected items.
+// If add_all is true, add up all items.
+pub fn sum_weights(items: &mut Vec<Item>, add_all: bool) -> i32 {
+    if add_all {
+        return items.iter().map(|item| item.weight).sum();
+    } else {
+        return items
+            .iter()
+            .filter(|item| item.is_selected)
+            .map(|item| item.weight)
+            .sum();
+    }
+}
+
+// Return the value of this solution.
+// If the solution is too heavy, return -1 so we prefer an empty solution.
+pub fn solution_value(items: &mut Vec<Item>, allowed_weight: i32) -> i32 {
+    // If the solution's total weight > allowed_weight,
+    // return -1 so even an empty solution is better.
+    if sum_weights(items, false) > allowed_weight {
+        return -1;
+    }
+
+    // Return the sum of the selected values.
+    return sum_values(items, false);
+}
+
+// Print the selected items.
+// If add_all is true, print all items
+pub fn print_selected(items: &Vec<Item>, all: bool) {
+    let mut num_printed = 0;
+    for i in 0..items.len() {
+        if items[i].is_selected || all {
+            println!("item {}: value = {}, weight = {}", i, items[i].value, items[i].weight);
+        }
+        num_printed += 1;
+        if num_printed > 100 {
+            println!("...");
+            return;
+        }
+    }
+    println!();
+}
+
--- knapsack/src/main.rs
+++ knapsack/src/main.rs
@@ -1,5 +1,66 @@
+#[warn(dead_code)]
+#[warn(unused_imports)]
 mod knapsack_random_generation;
+use crate::knapsack_random_generation::*;
+
+use std::time::Instant;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+// Run the algorithm. Display the elapsed time and solution.
+pub fn run_algorithm(
+    alg: &dyn Fn(&mut Vec<Item>, i32) -> (Vec<Item>, i32, i32),
+    items: &mut Vec<Item>,
+    allowed_weight: i32,
+) {
+    // Copy the items so the run isn't influenced by a previous run.
+    let mut test_items = copy_items(items);
+
+    let start = Instant::now();
+
+    // Run the algorithm.
+    let mut solution: Vec<Item>;
+    let total_value: i32;
+    let function_calls: i32;
+    (solution, total_value, function_calls) = alg(&mut test_items, allowed_weight);
+
+    let duration = start.elapsed();
+    println!("Elapsed: {:?}", duration);
+
+    print_selected(&mut solution, false);
+    println!(
+        "Value: {}, Weight: {}, Calls: {}",
+        total_value,
+        sum_weights(&mut solution, false),
+        function_calls
+    );
+    println!();
+}
 
 fn main() {
-    println!("Hello, world!");
+    // Prepare a Prng using the same seed each time.
+    let mut prng = Prng { seed: 1337 };
+
+    // Make some random items.
+    let mut items = make_items(
+        &mut prng, NUM_ITEMS, MIN_VALUE, MAX_VALUE, MIN_WEIGHT, MAX_WEIGHT,
+    );
+    let allowed_weight = sum_weights(&mut items, true) / 2;
+
+    // Display basic parameters.
+    println!("*** Parameters ***");
+    println!("# items:        {}", NUM_ITEMS);
+    println!("Total value:    {}", sum_values(&mut items, true));
+    println!("Total weight:   {}", sum_weights(&mut items, true));
+    println!("Allowed weight: {}", allowed_weight);
+    println!();
+
+    print_selected(&items, true);
+
+    // // Exhaustive search
+    // if NUM_ITEMS > 23 {    // Only run exhaustive search if num_items is small enough.
+    //     println!("Too many items for exhaustive search\n");
+    // } else {
+    //     println!("*** Exhaustive Search ***");
+    //     run_algorithm(&exhaustive_search, &mut items, allowed_weight);
+    // }
 }
