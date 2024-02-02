3a1d289637cf26db3b7eda53b0c22a156622f5b1
Ruben Heradio <rheradio@issi.uned.es>
2024-02-02 19:19:00 +0100

    Bread-first search

---
+++ graphs/src/breadth_first.rs
@@ -0,0 +1,31 @@
+use std::collections::HashMap;
+use std::collections::VecDeque;
+
+fn push_nodes<'a>(queue: &mut VecDeque<&'a String>, nodes: &'a Option<Vec<String>>) {
+    match nodes {
+        None => {}
+        Some(v) => {
+            for e in v {
+                queue.push_back(e);
+            }
+        }
+    }
+}
+pub fn breadth_first(graph: &HashMap<String, Option<Vec<String>>>, start_node: &String) {
+    println!("# Breadth-First Search from {} ###########", start_node);
+    let mut queue: VecDeque<&String> = VecDeque::new();
+    let mut searched: Vec<&String> = vec![];
+    println!("{}", start_node);
+    let mut nodes = graph.get(start_node).unwrap();
+    push_nodes(&mut queue, nodes);
+    let mut node: &String;
+    while !queue.is_empty() {
+        node = queue.pop_front().unwrap();
+        println!("{}", node);
+        if !searched.iter().any(|&i| i == node) {
+            nodes = graph.get(node).unwrap();
+            push_nodes(&mut queue, nodes);
+            searched.push(node);
+        }
+    }
+}
